5cbcae69d71158b91532b7d8d26aabbf1e93cfa1
Tobias Henkel <tobias_henkel@gmx.de>
2025-01-11 17:14:44 +0100

    Move main scripts to iot package
    This allows us to temporarily have different mains per stack. The dev
    stack will continue on synology where as the prod stack will deploy
    the same services on k8s.

--- __main__.py
+++ __main__.py
@@ -1,30 +1,11 @@
 import pulumi as p
-import pulumi_cloudflare as cloudflare
-import pulumi_docker as docker
 
-from iot.config import ComponentConfig
-from iot.mosquitto_legacy import create_mosquitto_legacy
-from iot.mqtt_exporter_legacy import create_mqtt_prometheus_legacy
+# Dev stack is the legacy stack
+if p.get_stack() == 'dev':
+    import iot.main_legacy
 
-component_config = ComponentConfig.model_validate(p.Config().get_object('config'))
+    iot.main_legacy.main()
+else:
+    import iot.main
 
-config = p.Config()
-stack = p.get_stack()
-org = p.get_organization()
-minio_stack_ref = p.StackReference(f'{org}/s3/dev')
-
-docker_provider = docker.Provider('synology', host='ssh://synology')
-
-opts = p.ResourceOptions(provider=docker_provider)
-
-cloudflare_provider = cloudflare.Provider(
-    'cloudflare',
-    api_key=component_config.cloudflare.api_key.value,
-    email=component_config.cloudflare.email,
-)
-
-# Create networks so we don't have to expose all ports on the host
-network = docker.Network('iot', opts=opts)
-
-create_mosquitto_legacy(component_config, network, cloudflare_provider, opts)
-create_mqtt_prometheus_legacy(component_config, network, opts)
+    iot.main.main()
